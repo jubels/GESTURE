adc7a7cfaeafc05c839e64bf952d3fa9bd9a6a98
Joske Ubels <joske.ubels@gmail.com>
2018-04-07 10:36:42 +0200

    Update gestureFinalClassifier.r

--- gestureFinalClassifier.r
+++ gestureFinalClassifier.r
@@ -5,8 +5,14 @@ FinalClassifier = function(surv, gep, sets, results, maxUse = 1000){
 	repeats = settings$repeats
 	toi = settings$toi
 	stratify = settings$stratify
-	### base meanHR on repeats from Run 1 
+	### base meanHR on repeats from Run 1
 	mean.HR <- apply(results.all[1,1:repeats,], 2, mean)
+	
+	if(any(is.na(apply(results.all[1,,],2,mean)))){
+		mean.HR = mean.HR[!is.na(apply(results.all[1,,],2,mean))]
+		class.c = class.c[,,!is.na(apply(results.all[1,,],2,mean))] 
+		warning(paste("NAs found in results; ", length(which(is.na(apply(results.all[1,,],2,mean)))), " geneset(s) excluded from analysis", sep = ""))
+	}	
 	surv[which(surv[,3] != toi),3] <- "a"
 	surv[which(surv[,3] == toi),3] <- "b"
 	
@@ -26,16 +32,16 @@ FinalClassifier = function(surv, gep, sets, results, maxUse = 1000){
 		surv.t <- surv[c(settings$folds[[(x+repeats)]][[1]], settings$folds[[(x+repeats)]][[2]]),]
 		surv.v <- surv[settings$folds[[(x+repeats)]][[3]],]
 		if(stratify == "YES"){
-			training[x,1,1] <- summary(coxph(Surv(surv[,1],surv[,2])~surv[,3] + strata(surv[,4]) , data = surv, subset = class.ab[,top] == 1))$coef[2]
-			training[x,1,2] <- summary(coxph(Surv(surv[,1],surv[,2])~surv[,3] + strata(surv[,4]) , data = surv, subset = class.ab[,top] == 1))$coef[5]
-			training[x,1,3] <- summary(coxph(Surv(surv[,1],surv[,2])~surv[,3] + strata(surv[,4]) , data = surv.v, subset = class.d[,top] == 1))$coef[2]
-			training[x,1,4] <- summary(coxph(Surv(surv[,1],surv[,2])~surv[,3] + strata(surv[,4]) , data = surv.v, subset = class.d[,top] == 1))$coef[5]
+			training[x,1,1] <- summary(coxph(Surv(surv.t[,1],surv.t[,2])~surv.t[,3] + strata(surv.t[,4]) , data = surv.t, subset = class.ab[,top] == 1))$coef[2]
+			training[x,1,2] <- summary(coxph(Surv(surv.t[,1],surv.t[,2])~surv.t[,3] + strata(surv.t[,4]) , data = surv.t, subset = class.ab[,top] == 1))$coef[5]
+			training[x,1,3] <- summary(coxph(Surv(surv.v[,1],surv.v[,2])~surv.v[,3] + strata(surv.v[,4]) , data = surv.v, subset = class.d[,top] == 1))$coef[2]
+			training[x,1,4] <- summary(coxph(Surv(surv.v[,1],surv.v[,2])~surv[.v,3] + strata(surv.v[,4]) , data = surv.v, subset = class.d[,top] == 1))$coef[5]
 		}
 		if(stratify == "NO"){
-			training[x,1,1] <- summary(coxph(Surv(surv[,1],surv[,2])~surv[,3], data = surv, subset = class.ab[,top] == 1))$coef[2]
-			training[x,1,2] <- summary(coxph(Surv(surv[,1],surv[,2])~surv[,3], data = surv, subset = class.ab[,top] == 1))$coef[5]
-			training[x,1,3] <- summary(coxph(Surv(surv[,1],surv[,2])~surv[,3], data = surv.v, subset = class.d[,top] == 1))$coef[2]
-			training[x,1,4] <- summary(coxph(Surv(surv[,1],surv[,2])~surv[,3], data = surv.v, subset = class.d[,top] == 1))$coef[5]
+			training[x,1,1] <- summary(coxph(Surv(surv.t[,1],surv.t[,2])~surv.t[,3], data = surv.t, subset = class.ab[,top] == 1))$coef[2]
+			training[x,1,2] <- summary(coxph(Surv(surv.t[,1],surv.t[,2])~surv.t[,3], data = surv.t, subset = class.ab[,top] == 1))$coef[5]
+			training[x,1,3] <- summary(coxph(Surv(surv.v[,1],surv.v[,2])~surv.v[,3], data = surv.v, subset = class.d[,top] == 1))$coef[2]
+			training[x,1,4] <- summary(coxph(Surv(surv.v[,1],surv.v[,2])~surv.v[,3], data = surv.v, subset = class.d[,top] == 1))$coef[5]
 		}	
 		
 		### add gene sets sequentially, based on their mean HR
@@ -56,12 +62,12 @@ FinalClassifier = function(surv, gep, sets, results, maxUse = 1000){
 				surv.t$solution[which(surv.t$score >= i)] <- 1
 				if(length(unique(surv.t[which(surv.t$solution == 1),3])) == 2 & length(unique(surv.t[which(surv.t$solution == 0),3])) == 2){
 					if(stratify == "YES"){
-						results.e[count,1] <- summary(coxph(Surv(surv[,1],surv[,2]) ~surv[,3] + strata(surv[,4]), data = surv.t, subset = solution == 1))$coef[2]	
-						results.e[count,2] <- summary(coxph(Surv(surv[,1],surv[,2]) ~surv[,3] + strata(surv[,4]), data = surv.t, subset = solution == 1))$coef[5]
+						results.e[count,1] <- summary(coxph(Surv(surv.t[,1],surv.t[,2]) ~surv.t[,3] + strata(surv.t[,4]), data = surv.t, subset = solution == 1))$coef[2]	
+						results.e[count,2] <- summary(coxph(Surv(surv.t[,1],surv.t[,2]) ~surv.t[,3] + strata(surv.t[,4]), data = surv.t, subset = solution == 1))$coef[5]
 					}
 					if(stratify == "NO"){
-						results.e[count,1] <- summary(coxph(Surv(surv[,1],surv[,2]) ~surv[,3], data = surv.t, subset = solution == 1))$coef[2]	
-						results.e[count,2] <- summary(coxph(Surv(surv[,1],surv[,2]) ~surv[,3], data = surv.t, subset = solution == 1))$coef[5]
+						results.e[count,1] <- summary(coxph(Surv(surv.t[,1],surv.t[,2]) ~surv.t[,3], data = surv.t, subset = solution == 1))$coef[2]	
+						results.e[count,2] <- summary(coxph(Surv(surv.t[,1],surv.t[,2]) ~surv.t[,3], data = surv.t, subset = solution == 1))$coef[5]
 					}	
 					results.e[count,3] <- mean(surv.t$solution)
 					results.e[count,4] <- i
@@ -69,7 +75,7 @@ FinalClassifier = function(surv, gep, sets, results, maxUse = 1000){
 			} 
     
     
-			cutoff <- results.e[which(results.e[,1] == min(results.e[which(results.e[,3] > 0.2),1]))[1], 4 ]
+			cutoff <- results.e[which(results.e[,1] == min(results.e[which(results.e[,3] > settings$min.size),1]))[1], 4 ]
     
     
 			surv.t$solution <- rep(0, nrow(surv.t))
@@ -77,16 +83,16 @@ FinalClassifier = function(surv, gep, sets, results, maxUse = 1000){
 			surv.v$solution <- rep(0, nrow(surv.v))
 			surv.v$solution[which(surv.v$score >= cutoff)] <- 1
 			if(stratify == "YES"){
-				training[x,g,1] <- summary(coxph(Surv(surv[,1],surv[,2])~surv[,3] + strata(surv[,4]), data = surv.t, subset = solution == 1))$coef[2]
-				training[x,g,2] <- summary(coxph(Surv(surv[,1],surv[,2])~surv[,3] + strata(surv[,4]), data = surv.t, subset = solution == 1))$coef[5]
-				training[x,g,3] <- summary(coxph(Surv(surv[,1],surv[,2])~surv[,3] + strata(surv[,4]), data = surv.v, subset = solution == 1))$coef[2]
-				training[x,g,4] <- summary(coxph(Surv(surv[,1],surv[,2])~surv[,3] + strata(surv[,4]), data = surv.v, subset = solution == 1))$coef[5]
+				training[x,g,1] <- summary(coxph(Surv(surv.t[,1],surv.t[,2])~surv.t[,3] + strata(surv.t[,4]), data = surv.t, subset = solution == 1))$coef[2]
+				training[x,g,2] <- summary(coxph(Surv(surv.t[,1],surv.t[,2])~surv.t[,3] + strata(surv.t[,4]), data = surv.t, subset = solution == 1))$coef[5]
+				training[x,g,3] <- summary(coxph(Surv(surv.v[,1],surv.v[,2])~surv.v[,3] + strata(surv.v[,4]), data = surv.v, subset = solution == 1))$coef[2]
+				training[x,g,4] <- summary(coxph(Surv(surv.v[,1],surv.v[,2])~surv.v[,3] + strata(surv.v[,4]), data = surv.v, subset = solution == 1))$coef[5]
 			}
 			if(stratify == "NO"){
-				training[x,g,1] <- summary(coxph(Surv(surv[,1],surv[,2])~surv[,3], data = surv.t, subset = solution == 1))$coef[2]
-				training[x,g,2] <- summary(coxph(Surv(surv[,1],surv[,2])~surv[,3], data = surv.t, subset = solution == 1))$coef[5]
-				training[x,g,3] <- summary(coxph(Surv(surv[,1],surv[,2])~surv[,3], data = surv.v, subset = solution == 1))$coef[2]
-				training[x,g,4] <- summary(coxph(Surv(surv[,1],surv[,2])~surv[,3], data = surv.v, subset = solution == 1))$coef[5]
+				training[x,g,1] <- summary(coxph(Surv(surv.t[,1],surv.t[,2])~surv.t[,3], data = surv.t, subset = solution == 1))$coef[2]
+				training[x,g,2] <- summary(coxph(Surv(surv.t[,1],surv.t[,2])~surv.t[,3], data = surv.t, subset = solution == 1))$coef[5]
+				training[x,g,3] <- summary(coxph(Surv(surv.v[,1],surv.v[,2])~surv.v[,3], data = surv.v, subset = solution == 1))$coef[2]
+				training[x,g,4] <- summary(coxph(Surv(surv.v[,1],surv.v[,2])~surv.v[,3], data = surv.v, subset = solution == 1))$coef[5]
 			}
 		
 		}
@@ -109,6 +115,10 @@ FinalClassifier = function(surv, gep, sets, results, maxUse = 1000){
 	#### select optimal number of gene sets
 	g = (which(delta < 0.0001)[1]) 
 	
+	if(is.na(g)){
+		g = maxUse
+		warning("No optimal performance found; maximum available number of genesets used")
+	}
 	#### base final ranking on the meanHR from Run2
 	mean.HR <- apply(results.all[1,(repeats+1):(repeats*2),], 2, mean)
 	
